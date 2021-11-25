ef1cbdc48f871bdf8351ff9fd704ebc498bf49d2
Sachin Pratap Singh <48355617+sachin2singh@users.noreply.github.com>
2021-11-25 11:38:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ express = require('express');
 
 app = express();
 const bodyParser = require('body-parser');
-const apiFeatureRouter = require ('./sls.node.ex.router/Router');
-const {WULIST_CONSTANT} = require ('./sls.node.ex.utils/constant');
-const {WULIST_TABLE} = require ('./sls.node.ex.utils/common');
+const apiEmpRouter = require ('./sls.node.ex.router/Router');
+const {LIST_CONSTANT} = require ('./sls.node.ex.utils/constant');
+const {LIST_TABLE} = require ('./sls.node.ex.utils/common');
 const dotenv = require('dotenv');
 const envVar = dotenv.config();
 if (envVar.error) throw new Error('envVar error', envVar.error);
 
 app.use(bodyParser.json());
-app.use('/api/v1/featureflags', apiFeatureRouter);
+app.use('/api/v1/Empflags', apiEmpRouter);
 
 module.exports = app;
